bfd89d456fea2f7c0885ccbc94a196fb421ada58
Ivan <nighteliteace@gmail.com>
2021-12-18 15:37:27 +0100

    Small cleanup.

--- NVTProjekat/src/app/app.module.ts
+++ NVTProjekat/src/app/app.module.ts
@@ -11,13 +11,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChefPageComponent } from './modules/order/pages/chef-page/chef-page.component';
-import { OrderItemTableViewComponent } from './modules/order/components/order-item-table-view/order-item-table-view.component';
-import { BartenderPageComponent } from './modules/order/pages/bartender-page/bartender-page.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { PinModalComponent } from './modules/order/components/pin-modal/pin-modal.component';
 import { OrderModule } from './modules/order/order.module';
 
 
