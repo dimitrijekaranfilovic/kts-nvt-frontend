da36348b99cad20dd09819420e7bc929f3d98a95
Dimitrije Karanfilovic <dimitrije.karanfilovic@gmail.com>
2021-12-22 21:01:06 +0100

    Improved PIN modal component.

--- NVTProjekat/src/app/modules/order/components/pin-modal/pin-modal.component.ts
+++ NVTProjekat/src/app/modules/order/components/pin-modal/pin-modal.component.ts
@@ -1,27 +1,40 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export interface DialogData {
-  pin: string
+  pin: string;
 }
 
 @Component({
   selector: 'app-pin-modal',
   templateUrl: './pin-modal.component.html',
-  styleUrls: ['./pin-modal.component.scss']
+  styleUrls: ['./pin-modal.component.scss'],
 })
 export class PinModalComponent implements OnInit {
-
+  form!: FormGroup;
   constructor(
+    private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<PinModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData,
-  ) {}
-
-  ngOnInit(): void {
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {
+    this.form = this.formBuilder.group({
+      pin: [this.data.pin, [Validators.required, Validators.minLength(1)]],
+    });
   }
+
+  ngOnInit(): void {}
 
   onNoClick(pin: string, event: string): void {
-    this.dialogRef.close({pin, event});
+    this.dialogRef.close({ pin, event });
   }
-
+  onCancelClick(): void {
+    this.dialogRef.close({ pin: this.data.pin, event: 'CANCEL' });
+  }
+  onSubmit(): void {
+    if (!this.form.valid) {
+      return;
+    }
+    this.dialogRef.close(this.form.value.pin);
+  }
 }
