5df0be6d824a0f1a922fb4d8ef99898db6f8589c
Dimitrije Karanfilovic <dimitrije.karanfilovic@gmail.com>
2021-12-28 17:03:50 +0100

    Implemented base64 image source in menu item card.

--- NVTProjekat/src/app/modules/menu-item/components/menu-item-card/menu-item-card.component.ts
+++ NVTProjekat/src/app/modules/menu-item/components/menu-item-card/menu-item-card.component.ts
@@ -2,17 +2,25 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { OrderItemGroupReducedInfo } from 'src/app/modules/order/types/OrderItemGroupReducedInfo';
 import { AddMenuItem } from '../../types/AddMenuItem';
 import { MenuItem } from '../../types/MenuItem';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-menu-item-card',
   templateUrl: './menu-item-card.component.html',
   styleUrls: ['./menu-item-card.component.scss'],
 })
-export class MenuItemCardComponent {
+export class MenuItemCardComponent implements OnInit {
+  public imageSource: SafeResourceUrl = '';
   @Input() menuItem!: MenuItem;
   @Input() groups: OrderItemGroupReducedInfo[] = [];
   @Output() menuItemAdded = new EventEmitter<AddMenuItem>();
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    this.imageSource = this.sanitizer.bypassSecurityTrustResourceUrl(
+      'data:image/jpg;base64,' + this.menuItem.itemImage
+    );
+  }
 
   addMenuItemToExistingGroup(group: OrderItemGroupReducedInfo): void {
     const data: AddMenuItem = {
